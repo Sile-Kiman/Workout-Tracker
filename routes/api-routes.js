61f7056a63178453277d94d3a7fa3dc70e48e8bb
Mamessile Kiman <sile.kiman@yahoo.com>
2020-02-20 23:46:59 -0500

    uploading some initial code for the assigment

---
+++ routes/api-routes.js
@@ -0,0 +1,25 @@
+ let mongoose = require("mongoose");
+let db = require("../models");
+
+module.exports = app => {
+
+  app.get("/", function(req, res) {
+     
+  res.send();
+ 
+  });
+
+  app.get("/stats", function(req, res) {
+     
+    res.send();
+     
+          
+    });
+
+    app.get("/exercise", function(req, res) {
+     
+      res.send();
+           
+      });  
+ 
+}
